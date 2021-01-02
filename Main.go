2c8825981766ff2a6cfd13826602e8b26b0e2fce
Matthew Sedlacek <sedlacek1991@gmail.com>
2021-01-02 11:44:30 -0800

    example of block variables at the package level

--- Main.go
+++ Main.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
+
+var (
+	playerName    string  = "Andre Agassi"
+	sponser       string  = "Head"
+	worldRanking  int     = 1
+	stringTension float32 = 65.5
+)
+
+var (
+	matchesPlayed int = 0
 )
 
 func main() {
-	// multi-line approach: useful for for loops
-	var i int
-	i = 14
-	// single-line: useful for when you need control over the data type
-	var j float32 = 15
-	// colon-equals: least verbose, use when Go's compiler is able to determine data type
-	k := 16
-	fmt.Println(i)
-	fmt.Printf("%v, %T", j, j)
-	fmt.Printf("%v, %T", k, k)
+	fmt.Println(playerName, sponser, worldRanking, stringTension, matchesPlayed)
 }
