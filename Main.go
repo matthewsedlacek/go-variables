308439e9ea1bc700c8977b34eb61c6872a2e9b9b
Matthew Sedlacek <sedlacek1991@gmail.com>
2021-01-02 11:09:44 -0800

    ways to declare variables in Golang

--- Main.go
+++ Main.go
@@ -5,6 +5,14 @@ import (
 )
 
 func main() {
+	// multi-line approach: useful for for loops
 	var i int
-	fmt.Println(42)
+	i = 14
+	// single-line: useful for when you need control over the data type
+	var j float32 = 15
+	// colon-equals: least verbose, use when Go's compiler is able to determine data type
+	k := 16
+	fmt.Println(i)
+	fmt.Printf("%v, %T", j, j)
+	fmt.Printf("%v, %T", k, k)
 }
