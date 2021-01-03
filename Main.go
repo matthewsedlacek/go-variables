96d8ab5a9af2fd18f0f460a808ff0e219c3c2bcf
Matthew Sedlacek <sedlacek1991@gmail.com>
2021-01-02 16:31:32 -0800

    use conversion operator to convert integer to float

--- Main.go
+++ Main.go
@@ -2,21 +2,13 @@ package main
 
 import "fmt"
 
-//uppercase: globally visible
-var I int = 27
-
-// lowercase: visibile to the package only
-var (
-	playerName    string  = "Andre Agassi"
-	sponsor       string  = "Head"
-	worldRanking  int     = 1
-	stringTension float32 = 65.5
-)
-
 func main() {
-	// block: only visible within the main function
 	var i int = 42
-	fmt.Println(i)
-	fmt.Println(I)
-	fmt.Println(playerName, sponsor, worldRanking, stringTension)
+	fmt.Printf("%v, %T\n", i, i)
+
+	var j float32
+	// conversion operator
+	j = float32(i)
+	fmt.Printf("%v, %T\n", j, j)
+
 }
