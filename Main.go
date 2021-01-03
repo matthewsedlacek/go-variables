691234e9abb40b51ae8e3ec47cfdefc573e65553
Matthew Sedlacek <sedlacek1991@gmail.com>
2021-01-02 19:14:07 -0800

    show how to convert integer to string using strconv package

--- Main.go
+++ Main.go
@@ -1,14 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var i int = 42
 	fmt.Printf("%v, %T\n", i, i)
 
-	var j float32
-	// conversion operator
-	j = float32(i)
+	var j string
+	// use strconv package's str converter function to change from an integer to a string
+	j = strconv.Itoa(i)
 	fmt.Printf("%v, %T\n", j, j)
 
 }
